58c1bb9cef8d6a9e14896f6945a2f9608f2ab89b
Shubh Sharma <iamshubh2@gmail.com>
2017-03-27 01:08:35 +0530

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -1,8 +1,36 @@
-window.setTimeout(FacebookLogout,150000);
+var timer;
+window.onblur = function () { 
+ 
+clearInterval(timer);
+   timer = window.setTimeout(FacebookLogout, 6000);
+
+}; 
+
+document.onclick= function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+document.onmousemove = function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+
+document.onkeydown = function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+
+window.onfocus = function () { 
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
 
 function FacebookLogout() {
 
-	var insertedNodes = [];
+	
 	
 	var logout;
     
@@ -39,10 +67,8 @@ function FacebookLogout() {
 	
 	} else {
 		
-		alert("we need to logout now");
+		//alert("we need to logout now");
 		logoutbutton.click();
 	}
     
 }
-     
-    
